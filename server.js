b66e7d235c10b1c5ac2e902db6f5d630c2ed7ded
marvel-soft <jimbo.sievers@gmail.com>
2017-04-04 08:35:21 -0700

    initial commit of node begginner tutorial

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
   var http = require('http');
-  http.createServer(function(request, response) {
+  function onRequest(request, response) {
+    console.log('request received');
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
     response.end();
-  }).listen(8888);
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('server has started');
+  console.log("buddy");
