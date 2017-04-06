dd4f1cd8fd5e166cd66a1d1d561ee951e10b0fce
marvel-soft <jimbo.sievers@gmail.com>
2017-04-05 22:10:44 -0700

    Fixed bad assignment of pathname

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
   var http = require('http');
-  function start() {
-    function onRequest(request, response) {
-      console.log('request received');
-      response.writeHead(200, {'Content-Type': 'text/plain'});
-      response.write('Hello World');
-      response.end();
-  }
-  http.createServer(onRequest).listen(8888);
-    console.log('server has started');
-}
+  var url = require('url');
 
-exports.start = start;
+  function start(route) {
+      function onRequest(request, response) {
+          var pathname = url.parse(request.url).pathname;
+          console.log('url: ' + request.url);
+          console.log('request for ' + pathname + ' received');
+          route(pathname);
+          response.writeHead(200, { 'Content-Type': 'text/plain' });
+          response.write('Hello World');
+          response.end();
+      }
+      http.createServer(onRequest).listen(8888);
+      console.log('server has started');
+  }
+
+  exports.start = start;
