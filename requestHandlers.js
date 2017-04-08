aa58f658abf5c737dd831b4eb4d6d2a6489140a2
marvel-soft <jimbo.sievers@gmail.com>
2017-04-08 06:51:48 -0700

    add requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,9 @@
 function start() {
-    console.log('request handler "start" was called');
+    console.log('request handler "start" was called.');
 }
 
 function upload() {
-    console.log('request handler "upload" was called.';)
+    console.log('request handler "upload" was called.');
 }
 
 exports.start = start;
